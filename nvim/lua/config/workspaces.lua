fcc69e56a94d2a662eaca32a6b4d27e15fea4020
Ian Forsyth <ian@ianforsyth.com>
2022-09-27 22:03:52 -0600

    Add Neovim configuration

---
+++ nvim/lua/config/workspaces.lua
@@ -0,0 +1,62 @@
+local status_ok, workspaces = pcall(require, "workspaces")
+if not status_ok then
+	return
+end
+
+local session_exists = function()
+   local f=io.open(".session.vim","r")
+   if f~=nil then io.close(f) return true else return false end
+end
+
+local set_working_directory = function()
+  vim.cmd('cd '..vim.fn.expand('%h')) -- Sets the working directory to whatever nvim was called on
+end
+
+local load_session = function()
+  os.execute('grep -v "argadd" .session.vim > .tmp_session.vim') -- Removing the launch argument to prevent an extra buffer
+  os.execute('mv .tmp_session.vim .session.vim')
+  vim.cmd('source .session.vim')
+end
+
+local save_session = function ()
+  if session_exists() then
+    vim.cmd('NvimTreeClose') -- NvimTree messes up saved buffers so just close it
+    vim.cmd('silent mksession! .session.vim')
+  end
+  vim.cmd('silent bufdo bw') -- Close all currently open buffers
+end
+
+local create_or_load_session = function()
+  if session_exists() then
+    load_session()
+  else
+    save_session()
+  end
+end
+
+vim.api.nvim_create_autocmd({"VimEnter"}, {
+  pattern = {"*"},
+  callback = function ()
+    set_working_directory()
+    if session_exists() then
+      load_session()
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd({"VimLeavePre"}, {
+  pattern = {"*"},
+  callback = save_session,
+})
+
+--[[ vim.api.nvim_create_user_command("WList", "WorkspacesList", {}) */ ]]
+--[[ vim.api.nvim_create_user_command("WAdd", "WorkspacesAdd <args>", { nargs = 1 }) */ ]]
+--[[ vim.api.nvim_create_user_command("WRemove", "WorkspacesRemove <args>", { nargs = 1 }) */ ]]
+--[[ vim.api.nvim_create_user_command("WRename", "WorkspacesRename <args>", { nargs = 1 }) */ ]]
+
+workspaces.setup({
+  hooks = {
+    open_pre = save_session,
+    open = create_or_load_session,
+  }
+})
